c6265f66f84134cb17273be1ba13524f2d3525c2
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-06 14:45:16 -0700

    SelectionStatements

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+
+    static void Main(String[] args)
+    {
+
+
+
+            Console.WriteLine("Hello, World!");
+
+
+
+    }
+}
