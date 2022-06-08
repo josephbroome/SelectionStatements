f3ac5336fae646604e1e7beb6ce72de28431d388
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-08 10:24:40 -0700

    udates

--- Program.cs
+++ Program.cs
@@ -7,7 +7,33 @@ class program
 
 
 
-            Console.WriteLine("Hello, World!");
+        int day = 0;
+        switch (day)
+        {
+            case 0:
+                Console.WriteLine("Sunday");
+                break;
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+        }
+
+
 
 
 
